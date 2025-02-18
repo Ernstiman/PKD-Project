92fc095edbe600e8629ce7ca0e9d76a6f9f79485
Viktor Lindfors <viktorlindfors0@gmail.com>
2025-02-18 14:10:45 +0100

    Wow canvas ritar en rect!

--- Code/main.js
+++ Code/main.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("hej");
+function draw() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+
+      ctx.fillStyle = "rgb(200 0 0)";
+      ctx.fillRect(10, 10, 500, 50);
+
+    }
+  }
+  draw();
