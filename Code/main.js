171aaec427015806ca8608632da57badd25d81f8
Viktor Forslund <viktor@ekastigen.net>
2025-02-18 14:27:41 +0100

    cirklar och streck

--- Code/main.js
+++ Code/main.js
@@ -7,6 +7,16 @@ function draw() {
       ctx.fillStyle = "rgb(200 0 0)";
       ctx.fillRect(10, 10, 500, 50);
 
+      ctx.beginPath();  // Start a new path
+        ctx.moveTo(50, 50);  // Start point (x, y)
+        ctx.lineTo(450, 450);  // End point (x, y)
+        ctx.stroke();  // Render the line
+
+        // Draw a circle
+        ctx.beginPath();  // Start a new path
+        ctx.arc(250, 250, 100, 0, 2 * Math.PI);  // x, y, radius, start angle, end angle
+        ctx.stroke();  // Render the circle
+
     }
   }
   draw();
